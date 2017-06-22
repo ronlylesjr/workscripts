a78b20f69dfcaabdfb3b4cc35c8eeb063fdeafa4
ronlylesjr <ronlyles.jr@gmail.com>
2017-06-22 14:45:15 -0700

    Removed findPageType()

--- metatagcheck.py
+++ metatagcheck.py
@@ -5,13 +5,6 @@ import requests
 import time
 import sys
 
-def findPageType(soup):
-    script = soup.find('script', attrs={'language' : 'javascript'})
-    pattern = re.compile("(\w+): (.*)")
-    fields = dict(re.findall(pattern, script.text))
-    pagetype = fields['PageType']
-    return pagetype 
-
 def scriptcheck(url):
     script = '<meta name="robots" content="noindex,follow,noodp" />'
     try:
@@ -34,15 +27,14 @@ def build_report(url):
 
     links = (x.get('href') for x in soup2.find('div', attrs={'id' : 'sitemap-content'}).findAll('a') if x.has_attr('href') and '#' not in x.get('href'))
     links = ('http:' + x if x[:1] == '/' else '' + x for x in links)
-    links = (x for x in links if scriptcheck(x) == 'Present')
-    results = set([x for x in links])
+    links = {x for x in links if scriptcheck(x) == 'Present'}
 
-    for i in results:
+    for i in links:
         print(i)
     print(time.time()-starttime)
 
 if __name__ == '__main__':
-    urllist = ['http://rontest1212015.beta.ebizautos.com/']
+    urllist = ['http://rontest1212015.beta.ebizautos.com/']#, 'http://www.hondaofspring.com/']
     start = time.time()
     for url in urllist:
         build_report(url)
