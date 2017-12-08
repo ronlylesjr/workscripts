5db19e36f34f0be951789da90f65ecc7f2b94d9b
ronlylesjr <ronlyles.jr@gmail.com>
2017-12-08 10:11:36 -0800

    Update metatagcheck.py

--- metatagcheck.py
+++ metatagcheck.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import concurrent.futures
 
 def scriptcheck(url):
     script = '<meta name="robots" content="noindex,follow,noodp" />'
@@ -12,22 +13,23 @@ def scriptcheck(url):
             scriptfinding = 'Not Present'
     except:
        scriptfinding = 'Broken Link'
-    return scriptfinding)   
+    return (url, scriptfinding)
 
 def build_report(url):
-    starttime = time.time()
     sitemap = url + 'sitemap.aspx'
     s = requests.Session()
     s.get(url)
     soup2 = BeautifulSoup((s.get(sitemap)).text, 'html.parser')
 
-    links = (x.get('href') for x in soup2.find('div', attrs={'id' : 'sitemap-content'}).findAll('a') if x.has_attr('href') and '#' not in x.get('href'))
-    links = ('http:' + x if x[:1] == '/' else '' + x for x in links)
-    links = {x for x in links if scriptcheck(x) == 'Present'}
+    links = (x.get('href') for x in soup2.find('div', 
+            attrs={'id' : 'sitemap-content'}).findAll('a') if x.has_attr('href') 
+            and '#' not in x.get('href'))
+    
+    links = ('http:' + x if x[:1] == '/' else x for x in links)
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+        result = executor.map(scriptcheck, links)
 
-    for i in links:
-        print(i)
-    print(time.time()-starttime)
+    print(set(x[0] for x in result if x[1] == 'Present'))
 
 if __name__ == '__main__':
     urllist = ['http://rontest1212015.beta.ebizautos.com/']
